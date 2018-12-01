1e721c3a662c1e8fdefd310cda155f714698463f
yanhuiz <zyh_840819@163.com>
2018-12-01 10:11:38 +0800

    asd

--- src/main/java/com/poto/anlab/model/User.java
+++ src/main/java/com/poto/anlab/model/User.java
@@ -2,16 +2,16 @@ package com.poto.anlab.model;
 
 public class User {
     private int userId;
-    private String user_name;
+    private String userName;
 
 
     public User(){
 
     }
 
-    public User(int userId, String user_name) {
+    public User(int userId, String userName) {
         this.userId = userId;
-        this.user_name = user_name;
+        this.userName = userName;
     }
 
     public int getUserId() {
@@ -22,11 +22,11 @@ public class User {
         this.userId = userId;
     }
 
-    public String getUser_name() {
-        return user_name;
+    public String getUserName() {
+        return userName;
     }
 
-    public void setUser_name(String user_name) {
-        this.user_name = user_name;
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
 }
